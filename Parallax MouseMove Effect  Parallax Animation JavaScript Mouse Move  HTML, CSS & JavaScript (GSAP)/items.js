693459ea76c690a1037fdf5e8c2c8fb62911cbfa
ferzuna <fadzil324@gmail.com>
2024-10-26 11:47:31 +0700

    Initial commit

---
+++ Parallax MouseMove Effect  Parallax Animation JavaScript Mouse Move  HTML, CSS & JavaScript (GSAP)/items.js
@@ -0,0 +1,65 @@
+const items = [
+    {
+        img: "./images/01.jpg",
+        icon: "<i class='ph ph-meta-logo'></i>",
+        link: "instagram.com",
+        parallaxSpeed: 0.065,
+    },
+    {
+        img: "./images/02.jpg",
+        icon: "<i class='ph ph-pinterest-logo'></i>",
+        link: "pinterest.com",
+        parallaxSpeed: 0.05,
+    },
+    {
+        img: "./images/03.jpg",
+        icon: "<i class='ph ph-pinterest-logo'></i>",
+        link: "pinterest.com",
+        parallaxSpeed: 0.08,
+    },
+    {
+        img: "./images/01.jpg",
+        icon: "<i class='ph ph-pinterest-logo'></i>",
+        link: "pinterest.com",
+        parallaxSpeed: 0.1,
+    },
+    {
+        img: "./images/02.jpg",
+        icon: "<i class='ph ph-pinterest-logo'></i>",
+        link: "pinterest.com",
+        parallaxSpeed: 0.07,
+    },
+    {
+        img: "./images/03.jpg",
+        icon: "<i class='ph ph-pinterest-logo'></i>",
+        link: "pinterest.com",
+        parallaxSpeed: 0.085,
+    },
+    {
+        img: "./images/01.jpg",
+        icon: "<i class='ph ph-pinterest-logo'></i>",
+        link: "pinterest.com",
+        parallaxSpeed: 0.06,
+    },
+    {
+        img: "./images/02.jpg",
+        icon: "<i class='ph ph-pinterest-logo'></i>",
+        link: "pinterest.com",
+        parallaxSpeed: 0.04,
+    },
+    {
+        img: "./images/03.jpg",
+        icon: "<i class='ph ph-pinterest-logo'></i>",
+        link: "pinterest.com",
+        parallaxSpeed: 0.1,
+    },
+    {
+        img: "./images/01.jpg",
+        icon: "<i class='ph ph-pinterest-logo'></i>",
+        link: "pinterest.com",
+        parallaxSpeed: 0.065,
+    },
+    
+];
+
+export {items};
